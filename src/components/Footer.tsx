e997eb6ee150045e91e1413181008ac9c32f36b3
Regine Kornbakk <reginekornbakk@gmail.com>
2025-05-29 15:11:12 +0200

    feat: edited footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,52 @@
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileLines } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 /**
  * React component that displays the footer.
  *
  * @returns {JSX.Element} The rendered footer component.
  */
-function Footer() {
+function Footer(): JSX.Element {
   return (
-    <footer className="bg-primary px-6 py-16 text-center md:py-20">
-      <p className="text-md block font-openSans opacity-80">
-        © 2025 Regine Kornbakk.
-      </p>
+    <footer className="bg-primary px-6 py-12 text-center md:py-16 bg-darkGray flex flex-col">
+      <p className="font-openSans font-semibold pb-4">Contact me at:</p>
+      <div className="items-center justify-center mx-auto flex flex-row gap-2">
+        <a
+          href="./rdk-cv-english.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center"
+        >
+          <FontAwesomeIcon
+            icon={faFileLines}
+            className="inline justify-center text-3xl"
+          />
+        </a>
+        <a
+          href="https://github.com/TheRegzi"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            className="inline justify-center ml-2 text-3xl"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/regine-dille-kornbakk-aa0a7b288/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            className="inline justify-center ml-2 text-3xl"
+          />
+        </a>
+      </div>
     </footer>
   )
 }
