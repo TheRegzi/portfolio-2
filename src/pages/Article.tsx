273c96f847499b720eea5d85f4f70b22d47ae07b
Regine Kornbakk <reginekornbakk@gmail.com>
2025-05-30 14:04:19 +0200

    feat: adding project data to article

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,7 +1,15 @@
+import { useParams } from 'react-router-dom'
+import projects from '../../data/projects.json'
+
 function Article() {
+  const { id } = useParams()
+  const project = projects.find((p) => String(p.id) === id)
+  if (!project)
+    return <div className="text-center text-red-500">Article not found.</div>
+
   return (
     <>
-      <h1>Hello article</h1>
+      <h1>{project.title}</h1>
     </>
   )
 }
